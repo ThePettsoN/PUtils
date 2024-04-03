36df7abebb72b02d79524c1cbe225bdbc7a5239d
Pontus Nilsson <me@nilssonpontus.com>
2024-04-04 00:51:30 +0200

    Fix version formatting error

--- src/core.lua
+++ src/core.lua
@@ -1,7 +1,7 @@
 local _, PUtils = ...
 
 local MAJOR = 1
-local MINOR = 2
-Putils.PATCH = string.format("PUtils-%d-%d", MAJOR, MINOR)
+local MINOR = 3
+Putils.PATCH = string.format("PUtils-%d.%d", MAJOR, MINOR)
 
 local Utils = LibStub and LibStub:NewLibrary(Putils.PATCH, MINOR)
