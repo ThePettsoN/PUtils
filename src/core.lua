cb963bf8735fb677c066066e4ebe67b3e9701b4d
Pontus Nilsson <me@nilssonpontus.com>
2024-05-18 23:28:08 +0200

    Update version

--- src/core.lua
+++ src/core.lua
@@ -1,7 +1,7 @@
 local _, PUtils = ...
 
 local MAJOR = 1
-local MINOR = 3
+local MINOR = 4
 PUtils.PATCH = string.format("PUtils-%d.%d", MAJOR, MINOR)
 
 local Utils = LibStub and LibStub:NewLibrary(PUtils.PATCH, MINOR)
