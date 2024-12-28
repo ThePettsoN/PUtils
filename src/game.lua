edd48b14e3c12a09748107575080e969d57b0edf
Pontus Nilsson <me@nilssonpontus.com>
2024-12-28 16:02:40 +0100

    Re-wrote and simplified logic a bit. Made it more consistent (#1)
    * Re-wrote and simplified logic a bit. Made it more consistent
    
    * Fix class lookup
    
    * Update CATA lookup to CATACLYSM

--- src/game.lua
+++ src/game.lua
@@ -1,145 +1,173 @@
 local _, PUtils = ...
-local Utils = LibStub:GetLibrary(PUtils.PATCH)
-if Utils.game then
+
+local Lib = PUtils.Library
+if not Lib then
     return
 end
 
 local GameUtils = {}
-Utils.game = GameUtils
+Lib.Game = GameUtils
 
-do
-	local gameVersion
-	local gameVersionIndex
+-- Lua APIs
+local strlower = strlower
 
-	GameUtils.GameVersionLookup = Utils.table.createLookup({
-		Vanilla = 1,
-		SeasonOfDiscovery = 2,
-		Hardcore = 3,
-		TBC = 4,
-		Wotlk = 5,
-		Cataclysm = 6,
-		Retail = 7,
-	})
+local TableUtils = PUtils.Library.Table
+local StringUtils = PUtils.Library.String
 
-	local function determineVersion()
-		if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
-			gameVersionIndex = GameUtils.GameVersionLookup.Retail
-		elseif WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC then
-			gameVersionIndex = GameUtils.GameVersionLookup.TBC
-		elseif WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC then
-			gameVersionIndex = GameUtils.GameVersionLookup.Wotlk
-		elseif WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC then
-			gameVersionIndex = GameUtils.GameVersionLookup.Cataclysm
-		elseif C_Engraving then
-			gameVersionIndex = GameUtils.GameVersionLookup.SeasonOfDiscovery
-		elseif C_GameRules.IsHardcoreActive() then
-			gameVersionIndex = GameUtils.GameVersionLookup.Hardcore
-		else
-			gameVersionIndex = GameUtils.GameVersionLookup.Vanilla
-		end
+local GameVersionLookup = TableUtils.createLookup({
+	CLASSIC = LE_EXPANSION_CLASSIC,
+	TBC = LE_EXPANSION_BURNING_CRUSADE,
+	WOTLK = LE_EXPANSION_WRATH_OF_THE_LICH_KING,
+	CATACLYSM = LE_EXPANSION_CATACLYSM,
+	MOP = LE_EXPANSION_MISTS_OF_PANDARIA,
+	WOD = LE_EXPANSION_WARLORDS_OF_DRAENOR,
+	LEGION = LE_EXPANSION_LEGION,
+	BFA = LE_EXPANSION_BATTLE_FOR_AZEROTH,
+	SHADOWLANDS = LE_EXPANSION_SHADOWLANDS,
+	DRAGONFLIGHT = LE_EXPANSION_DRAGONFLIGHT,
+	TWW = LE_EXPANSION_WAR_WITHIN,
+})
 
-		gameVersion = GameUtils.GameVersionLookup[gameVersionIndex]
+local SeasonLookup = TableUtils.createLookup({
+	NoSeason = 0,
+	SeasonOfMastery = 1,
+	SeasonOfDiscovery = 2,
+	Hardcore = 3,
+	Fresh = 4,
+	FreshHardcore = 12,
+})
+
+local ClassIdLookup = TableUtils.createLookup({
+	NONE = 0,
+	WARRIOR = 1,
+	PALADIN = 2,
+	HUNTER = 3,
+	ROGUE = 4,
+	PRIEST = 5,
+	DEATHKNIGHT = 6,
+	SHAMAN = 7,
+	MAGE = 8,
+	WARLOCK = 9,
+	MONK = 10,
+	DRUID = 11,
+	DEMONHUNTER = 12,
+	EVOKER = 13,
+})
+
+local ShapeshiftIdLookup = {
+	DRUID = TableUtils.createLookup({
+		AQUATIC_FORM = 2,
+		TRAVEL_FORM = 4,
+		FLIGHT_FORM = 5,
+		FLIGHT_FORM_BALANCE = 6
+	}),
+	SHAMAN = TableUtils.createLookup({
+		GHOST_WOLF = 1,
+	}),
+}
+
+local ItemRarityLookup = TableUtils.createLookup({
+	POOR = 0,
+	COMMON = 1,
+	UNCOMMON = 2,
+	RARE = 3,
+	EPIC = 4,
+	LEGENDARY = 5,
+	ARTIFACT = 6,
+	HEIRLOOM = 7,
+	TOKEN = 8,
+})
+
+GameUtils.GameVersionLookup = GameVersionLookup
+GameUtils.SeasonLookup = SeasonLookup
+GameUtils.ClassIdLookup = ClassIdLookup
+GameUtils.ShapeshiftIdLookup = ShapeshiftIdLookup
+GameUtils.ItemRarityLookup = ItemRarityLookup
+
+local classIndex, className
+local gameVersionIndex, gameVersion
+local seasonIndex, season
+local maxLevel
+
+local function determineGameData()
+	gameVersionIndex = LE_EXPANSION_LEVEL_CURRENT
+	gameVersion = GameVersionLookup[gameVersionIndex]
+
+	seasonIndex = C_Seasons and C_Seasons.GetActiveSeason() or 0
+	season = SeasonLookup[seasonIndex]
+
+	maxLevel = GetMaxLevelForExpansionLevel(gameVersionIndex)
+
+	className, classIndex = UnitClassBase("player")
+end
+determineGameData()
+
+GameUtils.GetGameVersion = function()
+	return gameVersion, GameVersionLookup[gameVersion]
+end
+GameUtils.CompareGameVersion = function(value)
+	local valueType = type(value)
+	if valueType == "number" then
+		return value <= gameVersionIndex
 	end
 
-	GameUtils.getGameVersion = function()
-		if not gameVersion then
-			determineVersion()
-		end
-
-		return gameVersion, gameVersionIndex
+	if valueType == "string" then
+		return GameVersionLookup[value] <= gameVersionIndex
 	end
 
-	GameUtils.compareGameVersion = function(index)
-		if not gameVersion then
-			determineVersion()
-		end
+	error("Invalid value type", value, valueType)
+end
+GameUtils.GetSeason = function()
+	return season, SeasonLookup[season]
+end
+GameUtils.CompareSeasion = function(value)
+	local valueType = type(value)
+	if valueType == "number" then
+		return value <= seasonIndex
+	end
 
-		return index == gameVersionIndex
+	if valueType == "string" then
+		return SeasonLookup[value] <= seasonIndex
+	end
+
+	error("Invalid value type")
+end
+GameUtils.GetMaxLevel = function()
+	return maxLevel
+end
+GameUtils.GetPlayerClass = function()
+	return className, classIndex
+end
+GameUtils.ComparePlayerClass = function (value)
+	local valueType = type(value)
+	if valueType == "number" then
+		return value == classIndex
+	end
+
+	if valueType == "string" then
+		return value == className
+	end
+
+	error("Invalid value type", value, valueType)
+end
+
+-- Generates functions to quickly check if player is on a specific expansion
+-- eg. IsClassic(), IsWotlk()
+for k, v in pairs(GameVersionLookup) do
+	if type(k) == "string" then
+		local gameVersionName = StringUtils.capitalize(strlower(k))
+		GameUtils["Is" .. gameVersionName] = function()
+			return v == gameVersionIndex
+		end
 	end
 end
 
-do
-	local gameExpansion
-	local gameExpansionIndex
-
-	GameUtils.GameExpansionLookup = Utils.table.createLookup({
-		Vanilla = 1,
-		Tbc = 2,
-		Wotlk = 3,
-		Cataclysm = 4,
-	})
-
-	local function determineExpansion()
-		local expansion = GetBuildInfo():sub(1,1)
-
-		gameExpansionIndex = tonumber(expansion)
-		gameExpansion = GameUtils.GameExpansionLookup[gameExpansionIndex]
-		if not gameExpansion and gameExpansionIndex >= 10 then
-			gameExpansion = "Retail"
-			GameUtils.GameExpansionLookup[gameExpansion] = gameExpansionIndex
-			GameUtils.GameExpansionLookup[gameExpansionIndex] = gameExpansion
+-- Generates functions to quickly check if player is on a specific expansion
+-- eg. IsHardcore(), IsSeasonOfDiscovery()
+for k, v in pairs(SeasonLookup) do
+	if type(k) == "string" then
+		GameUtils["Is" .. k] = function()
+			return v == seasonIndex
 		end
-	end
-
-	GameUtils.getGameExpansion = function()
-		if not gameExpansion then
-			determineExpansion()
-		end
-
-		return gameExpansion, gameExpansionIndex
-	end
-
-	GameUtils.compareGameExpansion = function(index)
-		if not gameExpansion then
-			determineExpansion()
-		end
-
-		if index < gameExpansionIndex then
-			return -1
-		end
-
-		if index == gameExpansionIndex then
-			return 0
-		end
-
-		return 1
 	end
 end
-
-GameUtils.ClassIds = {
-	None = 0,
-	Warrior = 1,
-	Paladin = 2,
-	Hunter = 3,
-	Rogue = 4,
-	Priest = 5,
-	DeathKnight = 6,
-	Shaman = 7,
-	Mage = 8,
-	Warlock = 9,
-	Druid = 11,
-}
-
-GameUtils.ShapeshiftIds = {
-	Druid = {
-		AquaticForm = 2,
-		TravelForm = 4,
-		FlightForm = 5,
-		FlightFormBalance = 6
-	},
-	Shaman = {
-		GhostWolf = 1,
-	},
-}
-
-GameUtils.ItemRarity = {
-	Poor = 0,
-	Common = 1,
-	Uncommon = 2,
-	Rare = 3,
-	Epic = 4,
-	Legendary = 5,
-	Artifact = 6,
-	Heirloom = 7,
-	Token = 8,
-}
